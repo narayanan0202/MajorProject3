e7907f6ebd42d4c86c389540d537f57a641c05e7
narayanan0202 <narayanan0202@gmail.com>
2018-12-22 15:05:21 +0530

    Initial commit

---
+++ base.py
@@ -0,0 +1,25 @@
+import nltk
+import numpy
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk import ne_chunk, pos_tag, pprint
+text = "hello world this is a simple test. Mr. Jack and Jill went up the hill"
+
+sents=sent_tokenize(text)
+
+print(sents)
+
+words = word_tokenize(text)
+print(words)
+
+print(nltk.wordpunct_tokenize(text))
+print(nltk.pos_tag(words))
+
+def entities(text):
+    return ne_chunk(
+        pos_tag(
+            word_tokenize(text)))
+
+
+tree = entities("Standing in the doorway of a Boeing 747, former president Barack Obama has one arm wrapped. ")
+tree.pprint()
+tree.draw()
